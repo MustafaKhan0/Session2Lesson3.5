6c1a9114e4b89f7bc60726ad5e09f7cf9b27d5b8
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2024-11-27 19:42:55 -0500

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+# Write a function `analyze_list(data)` that takes a list of numbers and prints:
+# 1. The sum of all elements.
+# 2. The average of the elements.
+# 3. The last element in the list.
+
+def analyze_list(data):
+  # Write your code here!
+
+
+# Testing
+list_1 = [4,5,6]
+list_2 = [4,6,7,12,4,67,54,2,9]
+
