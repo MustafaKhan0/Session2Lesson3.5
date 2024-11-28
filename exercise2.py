31b15c8cb1920705ec3e7c9242d45f0190112a1b
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2024-11-27 19:45:16 -0500

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+# Write a function `partial_average(prices, n)` that calculates the average of the first `n` days for a list of daily stock prices `prices`. 
+# The function should return a number, the average`
+
+def partial_average(prices, n):
+  # Write code here
+
+# Testing code
+print(partial_average([10, 20, 30, 40, 50], 3))
+print(partial_average([1, 2, 3, 4, 5, 6], 2))
+
+  
